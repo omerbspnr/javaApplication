84b2f9db6aa464cb7b424b975f259de11f5951cf
omerbspnr <baspinaromer1996@gmail.com>
2020-04-06 22:31:12 +0300

    5th May added

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -1,46 +1,35 @@
-/*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: Komut satırı argümanları ile veya klavyeden 
-----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import org.csystem.util.ArrayUtil;
-import org.csystem.util.Console;
-import org.csystem.util.StringUtil;
-
-import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Scanner;
-import static org.csystem.util.ArrayUtil.*;
-public class App {
+
+class App {
     public static void main(String[] args)
     {
-        int [] a = new int[]{Integer.MIN_VALUE};
-        int [] a1 = new int [] {Integer.MAX_VALUE};
-
-        System.out.println(Arrays.compareUnsigned(a, a1) );
-
     }
 }
+class Singleton {
+    private static Singleton ms_instance = null;
+    private Singleton(){}
 
-class Person {
-    private int m_age;
-    public Person(int age)
+    public static Singleton getInstance()
     {
-        m_age = age;
+        if (ms_instance == null)
+            ms_instance = new Singleton();
+
+        return ms_instance;
     }
+}
+class A
+{
+    private   int x;
+    public static class B {
+        private int x;
+        public A build()
+        {
+            A a = new A();
 
-    public int compare(Person other)
-    {
-        return m_age - other.m_age;
+            System.out.println(a.x);
+            return  a;
+        }
     }
-
-
-
-    public int getAge()
-    {
-        return m_age;
-    }
-
 }
